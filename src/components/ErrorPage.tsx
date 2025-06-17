8f58c1045881cab1e96d1f82e70e411e40b6ec71
clarks <leiferemil@gmail.com>
2025-06-17 18:59:53 +0300

    changed ErrorP

--- src/components/ErrorPage.tsx
+++ src/components/ErrorPage.tsx
@@ -12,7 +12,7 @@ const ErrorPage = () => {
 
 
     useEffect(() => {
-        if (performance.getEntriesByType("navigation")[0].type === "reload") {
+        if (performance.navigation.type === 1) {
             //if (performance.getEntriesByType("navigation")[0].type === "reload")
             navigate(Paths.HOME);
         }
